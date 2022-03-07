1a154a766167c702820370de66fc0ea04fc068a3
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-03-06 20:14:26 -0900

    ...

--- scripts/rebuild_linux.sh
+++ scripts/rebuild_linux.sh
@@ -13,7 +13,7 @@ rm -rf $BUILDROOT_DIR/output/build/linux-custom
 
 # Tar up the linux source and place it in the dl folder for buildroot to find it
 mkdir $BUILDROOT_DIR/dl
-mv archives/linux-4.19.218.tar.xz ${BUILDROOT_DIR}/dl/linux-4.19.218.tar.xz
+cp archives/linux-4.19.218.tar.xz ${BUILDROOT_DIR}/dl/linux-4.19.218.tar.xz
 
 # Finally move into the buildroot dir and call make
 cd $BUILDROOT_DIR
